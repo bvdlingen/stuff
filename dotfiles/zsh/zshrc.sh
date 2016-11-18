550ed74840862dccc8022d413cc0d174dacc178c
Federico Damián Schonborn <federico.d.schonborn@gmail.com>
2016-11-17 21:13:40 -0300

    Add Solus default compiler flags
    Signed-off-by: Federico Damián Schonborn <federico.d.schonborn@gmail.com>

--- dotfiles/zsh/zshrc.sh
+++ dotfiles/zsh/zshrc.sh
@@ -75,6 +75,28 @@ SAVEHIST=1000
 ## Ignore duplicates and lines starting with spaces
 HISTCONTROL=ignoreboth
 
+# Profiles
+## Default compiler flags
+if [ -z "${CFLAGS}" ]; then
+    export CFLAGS="-mtune=generic -march=x86-64 -ftree-vectorize -g2 -O2 -pipe -fPIC -Wformat -Wformat-security -fno-omit-frame-pointer -fstack-protector-strong --param ssp-buffer-size=4 -fexceptions -D_FORTIFY_SOURCE=2 -feliminate-unused-debug-types -Wno-error -Wp,-D_REENTRANT"
+fi
+
+if [ -z "${CXXFLAGS}" ]; then
+    export CXXFLAGS="-mtune=generic -march=x86-64 -ftree-vectorize -g2 -O2 -pipe -fPIC -Wformat -Wformat-security -fno-omit-frame-pointer -fstack-protector-strong --param ssp-buffer-size=4 -fexceptions -D_FORTIFY_SOURCE=2 -feliminate-unused-debug-types -Wno-error -Wp,-D_REENTRANT"
+fi
+
+if [ -z "${LDFLAGS}" ]; then
+    export LDFLAGS="-Wl,--copy-dt-needed-entries -Wl,-O1 -Wl,-z,relro -Wl,-z,now"
+fi
+
+if [ -z "${FCFLAGS}" ]; then
+    export FCFLAGS="${CFLAGS}"
+fi
+
+if [ -z "${FFLAGS}" ]; then
+    export FFLAGS="${CFLAGS}"
+fi
+
 # Functions
 ## Manage a lot of Git repositories
 function regit() {
