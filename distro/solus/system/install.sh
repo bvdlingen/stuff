c4c98c7d22da119936ff18141b67fbacd864b25e
Federico Damián Schonborn <federico.d.schonborn@gmail.com>
2016-11-23 08:33:43 -0300

    Fix SDS files
    Signed-off-by: Federico Damián Schonborn <federico.d.schonborn@gmail.com>

--- distro/solus/system/install.sh
+++ distro/solus/system/install.sh
@@ -1,9 +1,9 @@
 # Variables
 ## Set locations
-GITDIR="Git/start"
+GITDIR="Git/stuff"
 REPODIR="distro/solus/system"
 MAINDIR="$GITDIR/$REPODIR"
-DIRECTORIES=(/etc/lightdm /usr/share/icons/Arc-Paper)
+DIRECTORIES=(/etc/lightdm /usr/share/icons/default /usr/share/icons/Arc-Paper)
 
 # Create necessary folders
 for dir in ${DIRECTORIES[*]}; do
@@ -15,4 +15,5 @@ done
 # Copy files
 sudo cp ~/"$MAINDIR"/lightdm/lightdm.conf /etc/lightdm/lightdm.conf
 sudo cp ~/"$MAINDIR"/lightdm/greeter.conf /etc/lightdm/lightdm-gtk-greeter.conf
-sudo cp ~/"$MAINDIR"/icons/Arc-Paper.theme /usr/share/icons/Arc-Paper
+sudo cp ~/"$MAINDIR"/themes/default.cursor /usr/share/icons/default/index.theme
+sudo cp ~/"$MAINDIR"/themes/arc-paper.icons /usr/share/icons/Arc-Paper/index.theme
