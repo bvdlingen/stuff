94bef44a1336da804525b9a2b7623a601af754ab
Federico Damián Schonborn <federico.d.schonborn@protonmail.com>
2016-12-17 19:35:20 -0300

    Remove artwork changing files
    Signed-off-by: Federico Damián Schonborn <federico.d.schonborn@protonmail.com>

--- system/bootstrap.sh
+++ system/bootstrap.sh
@@ -9,5 +9,3 @@ FILES_DIR="$HOME/Git/stuff/system"
 
 # Copy files
 sudo install -Dm644 "$FILES_DIR/lightdm/lightdm.conf" /etc/lightdm/lightdm.conf
-sudo install -Dm644 "$FILES_DIR/lightdm/greeter.conf" /etc/lightdm/lightdm-gtk-greeter.conf
-sudo install -Dm644 "$FILES_DIR/themes/default.cursor" /usr/share/icons/default/index.theme
