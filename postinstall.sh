c735b8045d27828dfeb290f9435df533e064d5d3
Federico D. Schonborn <federico.d.schonborn@gmail.com>
2017-02-01 16:04:16 -0300

    Fix this
    Signed-off-by: Federico D. Schonborn <federico.d.schonborn@gmail.com>

--- postinstall.sh
+++ postinstall.sh
@@ -14,7 +14,7 @@ function print_step() {
     notify-send "Solus Post Install" "$message" -i distributor-logo-solus
 }
 
-function folder_enter() {
+function enter_folder() {
     folder="$1"
 
     if [ ! -d "$folder" ]; then
@@ -23,45 +23,54 @@ function folder_enter() {
     cd "$folder" || exit
 }
 
-function clone_repo() {
+function repo_clone() {
     repo="$1"
 
-    hub clone --recursive "$repo"
+    git clone --recursive "$repo"
 }
 
-function tpkg_from_list() {
+function tpkg_list() {
     third_list="$1"
 
     wget "$third_list" -O list.txt
     while ISC='' read -r tpkg_dir || [ -n "$tpkg_dir" ]; do
-        sudo eopkg build -y --ignore-safety https://raw.githubusercontent.com/solus-project/3rd-party/master/"$tpkg_dir"/pspec.xml
-        sudo eopkg install -y ./*.eopkg
+        sudo eopkg build -y --ignore-safety https://raw.githubusercontent.com/solus-project/3rd-party/master/"$tpkg_dir"/pspec.xml && sudo eopkg install -y ./*.eopkg
         rm -rfv ./*.eopkg
     done < list.txt
     rm -rfv list.txt
 }
 
-function clone_from_list() {
+function clone_list() {
     repo_list="$1"
 
     wget "$repo_list" -O list.txt
     while ISC='' read -r git_repo || [ -n "$git_repo" ]; do
-        clone_repo "$git_repo"
+        repo_clone "$git_repo"
     done < list.txt
     rm -rfv list.txt
 }
 
-function sources_from_list() {
+function github_list() {
+    github_list="$1"
+
+    wget "$github_list" -O list.txt
+    while ISC='' read -r github_repo || [ -n "$github_repo" ]; do
+        repo_clone https://github.com/"$github_repo"
+    done < list.txt
+    rm -rfv list.txt
+}
+
+function sources_list() {
     source_list="$1"
 
     wget "$source_list" -O list.txt
     while ISC='' read -r source_repo || [ -n "$source_repo" ]; do
-        clone_repo https://git.solus-project.com/packages/"$source_repo"
+        repo_clone https://git.solus-project.com/packages/"$source_repo"
     done < list.txt
     rm -rfv list.txt
 }
 
-function go_get_from_list() {
+function go_get_list() {
     pkgs_list="$1"
 
     wget "$pkgs_list" -O list.txt
@@ -75,20 +84,17 @@ function go_get_from_list() {
 print_step "Script is now running, do not touch anything until it finishes :)"
 
 # Password-less user
+print_step "Setting password-less user (EXTREMELY INSANE STUFF)"
 ## Remove password for Casa
-print_step "Setting password-less user"
 sudo passwd -du casa
 ## Add nullok option to PAM files
-print_step "Adding nullok option to PAM files (EXTREMELY INSANE STUFF)"
-sudo sed -e "s/sha512 shadow try_first_pass nullok/sha512 shadow try_first_pass/g" -i /etc/pam.d/system-password
-sudo sed -e "s/pam_unix.so/pam_unix.so nullok/g" -i /etc/pam.d/*
+sudo sed -e "s/sha512 shadow try_first_pass nullok/sha512 shadow try_first_pass/g" -e "s/pam_unix.so/pam_unix.so nullok/g" -i /etc/pam.d/*
 
 # Manage repositories
+print_step "Switching to Unstable repository"
 ## Remove Solus (Shannon)
-print_step "Removing repository Solus"
 sudo eopkg remove-repo -y Solus
 ## Add Unstable
-print_step "Adding Unstable repository"
 sudo eopkg add-repo -y Solus https://packages.solus-project.com/unstable/eopkg-index.xml.xz
 
 # Manage packages
@@ -100,56 +106,61 @@ print_step "Getting system up to date"
 sudo eopkg upgrade -y
 ## Install third party stuff
 print_step "Installing third party packages"
-tpkg_from_list "$LISTS_RAW_URL/third_party.txt"
+tpkg_list "$LISTS_RAW_URL/third_party.txt"
 ## Install extra applications and stuff
 print_step "Installing more packages"
-sudo eopkg install -y caja-extensions galculator simplescreenrecorder kodi geary libreoffice-all lutris zsh git{,-extras} hub yadm {python-,}neovim hugo golang yarn neofetch solbuild{,-config-unstable} cve-check-tool
+sudo eopkg install -y caja-extensions galculator simplescreenrecorder kodi geary libreoffice-all lutris zsh git{,-extras} yadm {python-,}neovim hugo golang nodejs python3-devel neofetch solbuild{,-config-unstable}
 
 # Development packages and Solbuild
 ## Install development component
 print_step "Installing development component and extra development packages"
 sudo eopkg install -y -c system.devel
-sudo eopkg install -y python3-devel
 ## Set up solbuild
 print_step "Setting up solbuild"
 sudo solbuild init -u
 
 # Dotfiles
 print_step "Setting-up dotfiles"
-## Cleanup
+## Fixes
+### Clean already present files
 rm -rfv ~/.config/gtk-3.0/bookmarks
 ## Clone the repository and decrypt the binary
 yadm clone -f https://github.com/feskyde/dotfiles
 yadm decrypt
 ## Default to ZSH
-print_step "Setting default shell"
 sudo chsh -s /bin/zsh casa
 
 # Git repositories
-folder_enter ~/Git
+print_step "Cloning repositories"
+enter_folder ~/Git
 ## GitHub repositories
-print_step "Cloning GitHub repositories"
-clone_from_list "$LISTS_RAW_URL/github_repos.txt"
+github_list "$LISTS_RAW_URL/github_repos.txt"
 ## Extra repositories
-print_step "Cloning extra repositories"
-clone_from_list "$LISTS_RAW_URL/extra_repos.txt"
+clone_list "$LISTS_RAW_URL/extra_repos.txt"
+## Return to home
+cd ~ || exit
+
+# System configuration
+print_step "Installing system configuration files"
+enter_folder ~/Git/solus-stuff/config
+bash bootstrap.sh
 ## Return to home
 cd ~ || exit
 
 # Solus packaging repository
 ## Create packages folder
 print_step "Setting up Solus packages folder"
-folder_enter ~/Git/packages
+enter_folder ~/Git/packages
 ## Clone common repository
 while true; do
     if [ ! -d common ]; then
-        clone_repo https://git.solus-project.com/common
+        repo_clone https://git.solus-project.com/common
     else
         break
     fi
 done
 ## Clone source repositories
-sources_from_list "$LISTS_RAW_URL"/solus_repos.txt
+sources_list "$LISTS_RAW_URL"/solus_repos.txt
 ## Link makefiles
 print_step "Linking makefiles"
 ln -srfv common/Makefile.common Makefile.common
@@ -158,17 +169,10 @@ ln -srfv common/Makefile.toplevel Makefile
 ## Return to home
 cd ~ || exit
 
-# System configuration
-print_step "Installing system configuration files"
-folder_enter ~/Git/solus-stuff/config
-bash bootstrap.sh
-## Return to home
-cd ~ || exit
-
 # Telegram Desktop
 print_step "Installing Telegram Desktop"
 ## Enter into the Telegram folder
-folder_enter ~/.TelegramDesktop
+enter_folder ~/.TelegramDesktop
 ## Download the tarball
 wget https://tdesktop.com/linux/current?alpha=1 -O telegram-desktop.tar.xz
 ## Unpack it
@@ -179,18 +183,18 @@ cd ~ || exit
 
 # Deezloader App
 print_step "Setting-up Deezloader App"
-folder_enter ~/Git/deezloader-app
-yarn install
+enter_folder ~/Git/deezloader-app
+npm install
 ## Back to home
 cd ~ || exit
 
 # Go packages
+print_step "Installing Go packages"
 ## Fixes
 ### Export GOPATH so the Go packages installation will not explode
 export GOPATH=$HOME/.golang
 ## Install packages
-print_step "Installing Go packages"
-go_get_from_list "$LISTS_RAW_URL/go_packages.txt"
+go_get_list "$LISTS_RAW_URL/go_packages.txt"
 
 # FINISHED!
 print_step "Script has finished! You should reboot as soon as possible"
