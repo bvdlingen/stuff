2e943d9651ee2fb60a7ece84738a1b1c37bf692a
Federico D. Schonborn <federico.d.schonborn@gmail.com>
2017-02-16 09:59:10 -0300

    Avoid errors where it's possible
    Signed-off-by: Federico D. Schonborn <federico.d.schonborn@gmail.com>

--- postinstall.sh
+++ postinstall.sh
@@ -9,14 +9,14 @@ LISTS_RAW_URL="https://raw.githubusercontent.com/feskyde/solus-stuff/master/list
 SPECS_RAW_URL="https://raw.githubusercontent.com/solus-project/3rd-party/master"
 
 # Functions
-function print_step() {
+function new_step_notify() {
     message="$1"
 
     printf "\e[1m>> %s\e[0m\n" "$message"
     notify-send "Solus Post Install" "$message" -i distributor-logo-solus
 }
 
-function enter_folder() {
+function checkout_folder() {
     folder="$1"
 
     if [ ! -d "$folder" ]; then
@@ -25,58 +25,51 @@ function enter_folder() {
     cd "$folder" || exit
 }
 
-function repo_clone() {
-    repo="$1"
-
-    git clone --recursive "$repo"
-}
-
-function tpkg_list() {
+function third_party_install_from_list() {
     third_list="$1"
 
-    wget "$third_list" -O list.txt
+    wget "$third_list" -O list_tpkg.txt
     while ISC='' read -r tpkg_dir || [ -n "$tpkg_dir" ]; do
         sudo eopkg build -y --ignore-safety "$SPECS_RAW_URL"/"$tpkg_dir"/pspec.xml
         sudo eopkg install -y ./*.eopkg
         rm -rfv ./*.eopkg
-    done < list.txt
-    rm -rfv list.txt
+    done < list_tpkg.txt
+    rm -rfv list_tpkg.txt
 }
 
-function clone_list() {
+function clone_repositories_from_list() {
     repo_list="$1"
-    repo_url="$2"
 
-    wget "$repo_list" -O list.txt
+    wget "$repo_list" -O list_clone.txt
     while ISC='' read -r git_repo || [ -n "$git_repo" ]; do
-        repo_clone "$repo_url$git_repo"
-    done < list.txt
-    rm -rfv list.txt
+        git clone --recursive "$git_repo"
+    done < list_clone.txt
+    rm -rfv list_clone.txt
 }
 
-function go_get_list() {
+function go_get_packages_from_list() {
     pkgs_list="$1"
 
-    wget "$pkgs_list" -O list.txt
+    wget "$pkgs_list" -O list_go_get.txt
     while ISC='' read -r gpkg_path || [ -n "$gpkg_path" ]; do
         go get -v -u "$gpkg_path"
-    done < list.txt
-    rm -rfv list.txt
+    done < list_go_get.txt
+    rm -rfv list_go_get.txt
 }
 
 # Welcome
-print_step "Script is now running, do not touch anything until it finishes :)"
+new_step_notify "Script is now running, do not touch anything until it finishes :)"
 
 # Password-less user
-print_step "Setting password-less user (EXTREMELY INSANE STUFF)"
+new_step_notify "Setting password-less user (EXTREMELY INSANE STUFF)"
 ## Remove password for Casa
 sudo passwd -du casa
 ## Add nullok option to PAM files
-sudo find /etc/pam.d/* -exec sed -i {} -e "s/try_first_pass nullok/try_first_pass/g" \
-                                       -e "s/pam_unix.so/pam_unix.so nullok/g" \;
+sudo find /etc/pam.d -name "*" -exec sed -i {} -e "s:try_first_pass nullok:try_first_pass:g" \
+                                               -e "s:pam_unix.so:pam_unix.so nullok:g" \;
 
 # Manage repositories
-print_step "Switching to Unstable repository"
+new_step_notify "Switching to Unstable repository"
 ## Remove Solus (Shannon)
 sudo eopkg remove-repo -y Solus
 ## Add Unstable
@@ -84,26 +77,26 @@ sudo eopkg add-repo -y Solus https://packages.solus-project.com/unstable/eopkg-i
 
 # Manage packages
 ## Upgrade the system
-print_step "Getting system up to date"
+new_step_notify "Getting system up to date"
 sudo eopkg upgrade -y
 ## Install third party stuff
-print_step "Installing third party packages"
-tpkg_list "$LISTS_RAW_URL/third_party.txt"
+new_step_notify "Installing third party packages"
+third_party_install_from_list "$LISTS_RAW_URL/third_party.txt"
 ## Install extra applications and stuff
-print_step "Installing more packages"
+new_step_notify "Installing more packages"
 sudo eopkg install -y caja-extensions galculator simplescreenrecorder libreoffice-all zsh \
-                      git{,-extras} hub yarn neofetch yadm golang solbuild{,-config-unstable}
+                      git{,-extras} yadm neofetch golang yarn solbuild{,-config-unstable}
 
 # Development packages and Solbuild
 ## Install development component
-print_step "Installing development component and extra development packages"
+new_step_notify "Installing development component and extra development packages"
 sudo eopkg install -y -c system.devel
 ## Set up solbuild
-print_step "Setting up solbuild"
+new_step_notify "Setting up solbuild"
 sudo solbuild init -u
 
 # Dotfiles
-print_step "Setting-up dotfiles"
+new_step_notify "Setting-up dotfiles"
 ## Fixes
 ### Clean already present files
 rm -rfv ~/.config/gtk-3.0/bookmarks
@@ -114,59 +107,50 @@ yadm decrypt
 sudo chsh -s /bin/zsh casa
 
 # Git repositories
-print_step "Cloning repositories"
-enter_folder ~/Git
+new_step_notify "Cloning repositories"
+checkout_folder ~/Git
 ## GitHub repositories
-clone_list "$LISTS_RAW_URL/github_repos.txt" https://github.com/
+clone_repositories_from_list "$LISTS_RAW_URL/github_repos.txt"
 ## Extra repositories
-clone_list "$LISTS_RAW_URL/extra_repos.txt"
-## Return to home
-cd ~ || exit
-
-# System configuration
-print_step "Installing system configuration files"
-enter_folder ~/Git/solus-stuff/config
-bash bootstrap.sh
+clone_repositories_from_list "$LISTS_RAW_URL/extra_repos.txt"
 ## Return to home
 cd ~ || exit
 
 # Solus packaging repository
 ## Create packages folder
-print_step "Setting up Solus packages folder"
-enter_folder ~/Git/packages
+new_step_notify "Setting up Solus packages folder"
+checkout_folder ~/Git/packages
 ## Clone common repository
-print_step "Clonning common repository"
+new_step_notify "Clonning common repository"
 while true; do
     if [ ! -d common ]; then
-        repo_clone https://git.solus-project.com/common
+        git clone --recursive https://git.solus-project.com/common
     else
         break
     fi
 done
 ## Link makefiles
-print_step "Linking makefiles"
+new_step_notify "Linking makefiles"
 ln -srfv common/Makefile.common Makefile.common
 ln -srfv common/Makefile.iso Makefile.iso
 ln -srfv common/Makefile.toplevel Makefile
 ## Clone all source repositories
-print_step "Clonning all source repositories"
+new_step_notify "Clonning all source repositories"
 make clone
 ## Return to home
 cd ~ || exit
 
-# Package Control
-print_step "Installing package control"
-## Enter to Sublime packages folder
-enter_folder ~/.config/sublime-text-3/Installed\ Packages
-## Download the package
-wget https://packagecontrol.io/Package%20Control.sublime-package -O Package\ Control.sublime-package
-## Back to home
+# System configuration
+new_step_notify "Installing system configuration files"
+checkout_folder ~/Git/solus-stuff/config
+bash bootstrap.sh
+## Return to home
 cd ~ || exit
 
 # Telegram Desktop
-print_step "Installing Telegram Desktop"
+new_step_notify "Installing Telegram Desktop"
 ## Enter into the Telegram folder
-enter_folder ~/.TelegramDesktop
+checkout_folder ~/.TelegramDesktop
 ## Download the tarball
 wget https://tdesktop.com/linux/current?alpha=1 -O telegram-desktop.tar.xz
 ## Unpack it
@@ -175,22 +159,31 @@ rm -rfv telegram-desktop.tar.xz
 ## Back to home
 cd ~ || exit
 
-# Deezloader App
-print_step "Setting-up Deezloader App"
-enter_folder ~/Git/deezloader-app
-yarn install
+# Package Control
+new_step_notify "Installing package control"
+## Enter to Sublime packages folder
+checkout_folder ~/.config/sublime-text-3/Installed\ Packages
+## Download the package
+wget https://packagecontrol.io/Package%20Control.sublime-package -O Package\ Control.sublime-package
 ## Back to home
 cd ~ || exit
 
 # Go packages
-print_step "Installing Go packages"
+new_step_notify "Installing Go packages"
 ## Fixes
 ### Export GOPATH so the Go packages installation will not go KABOOM!
 export GOPATH=$HOME/.golang
 ## Install packages
-go_get_list "$LISTS_RAW_URL/go_packages.txt"
+go_get_packages_from_list "$LISTS_RAW_URL/go_packages.txt"
 ## Install gometalinter linters
 $GOPATH/bin/gometalinter --install
 
+# Deezloader App
+new_step_notify "Setting-up Deezloader App"
+checkout_folder ~/Git/deezloader-server
+yarn install
+## Back to home
+cd ~ || exit
+
 # FINISHED!
-print_step "Script has finished! You should reboot as soon as possible"
+new_step_notify "Script has finished! You should reboot as soon as possible"
