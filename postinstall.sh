2e178e1979252733a0432c5e5540268f2c512c56
Federico D. Schonborn <federico.d.schonborn@gmail.com>
2017-02-15 22:33:12 -0300

    Save this now, I fucked up PAM
    Signed-off-by: Federico D. Schonborn <federico.d.schonborn@gmail.com>

--- postinstall.sh
+++ postinstall.sh
@@ -5,6 +5,8 @@
 
 # Lists (bad design is bad)
 LISTS_RAW_URL="https://raw.githubusercontent.com/feskyde/solus-stuff/master/lists"
+# Third Party specs
+SPECS_RAW_URL="https://raw.githubusercontent.com/solus-project/3rd-party/master"
 
 # Functions
 function print_step() {
@@ -34,7 +36,8 @@ function tpkg_list() {
 
     wget "$third_list" -O list.txt
     while ISC='' read -r tpkg_dir || [ -n "$tpkg_dir" ]; do
-        sudo eopkg build -y --ignore-safety https://raw.githubusercontent.com/solus-project/3rd-party/master/"$tpkg_dir"/pspec.xml && sudo eopkg install -y ./*.eopkg
+        sudo eopkg build -y --ignore-safety "$SPECS_RAW_URL"/"$tpkg_dir"/pspec.xml
+        sudo eopkg install -y ./*.eopkg
         rm -rfv ./*.eopkg
     done < list.txt
     rm -rfv list.txt
@@ -42,30 +45,11 @@ function tpkg_list() {
 
 function clone_list() {
     repo_list="$1"
+    repo_url="$2"
 
     wget "$repo_list" -O list.txt
     while ISC='' read -r git_repo || [ -n "$git_repo" ]; do
-        repo_clone "$git_repo"
-    done < list.txt
-    rm -rfv list.txt
-}
-
-function github_list() {
-    hub_list="$1"
-
-    wget "$hub_list" -O list.txt
-    while ISC='' read -r github_repo || [ -n "$github_repo" ]; do
-        repo_clone https://github.com/"$github_repo"
-    done < list.txt
-    rm -rfv list.txt
-}
-
-function sources_list() {
-    source_list="$1"
-
-    wget "$source_list" -O list.txt
-    while ISC='' read -r source_repo || [ -n "$source_repo" ]; do
-        repo_clone https://git.solus-project.com/packages/"$source_repo"
+        repo_clone "$repo_url$git_repo"
     done < list.txt
     rm -rfv list.txt
 }
@@ -88,7 +72,8 @@ print_step "Setting password-less user (EXTREMELY INSANE STUFF)"
 ## Remove password for Casa
 sudo passwd -du casa
 ## Add nullok option to PAM files
-sudo sed -e "s/sha512 shadow try_first_pass nullok/sha512 shadow try_first_pass/g" -e "s/pam_unix.so/pam_unix.so nullok/g" -i /etc/pam.d/*
+sudo find /etc/pam.d/* -exec sed -i {} -e "s/try_first_pass nullok/try_first_pass/g" \
+                                       -e "s/pam_unix.so/pam_unix.so nullok/g" \;
 
 # Manage repositories
 print_step "Switching to Unstable repository"
@@ -98,9 +83,6 @@ sudo eopkg remove-repo -y Solus
 sudo eopkg add-repo -y Solus https://packages.solus-project.com/unstable/eopkg-index.xml.xz
 
 # Manage packages
-## Remove unneded stuff
-print_step "Removing unneded stuff"
-sudo eopkg remove -y --purge firefox thunderbird arc-firefox-theme
 ## Upgrade the system
 print_step "Getting system up to date"
 sudo eopkg upgrade -y
@@ -109,7 +91,8 @@ print_step "Installing third party packages"
 tpkg_list "$LISTS_RAW_URL/third_party.txt"
 ## Install extra applications and stuff
 print_step "Installing more packages"
-sudo eopkg install -y caja-extensions galculator simplescreenrecorder kodi geary libreoffice-all lutris zsh git{,-extras} hub yadm {python-,}neovim hugo golang nodejs python3-devel neofetch solbuild{,-config-unstable}
+sudo eopkg install -y caja-extensions galculator simplescreenrecorder libreoffice-all zsh \
+                      git{,-extras} hub yarn neofetch yadm golang solbuild{,-config-unstable}
 
 # Development packages and Solbuild
 ## Install development component
@@ -134,7 +117,7 @@ sudo chsh -s /bin/zsh casa
 print_step "Cloning repositories"
 enter_folder ~/Git
 ## GitHub repositories
-github_list "$LISTS_RAW_URL/github_repos.txt"
+clone_list "$LISTS_RAW_URL/github_repos.txt" https://github.com/
 ## Extra repositories
 clone_list "$LISTS_RAW_URL/extra_repos.txt"
 ## Return to home
@@ -152,6 +135,7 @@ cd ~ || exit
 print_step "Setting up Solus packages folder"
 enter_folder ~/Git/packages
 ## Clone common repository
+print_step "Clonning common repository"
 while true; do
     if [ ! -d common ]; then
         repo_clone https://git.solus-project.com/common
@@ -159,14 +143,24 @@ while true; do
         break
     fi
 done
-## Clone source repositories
-sources_list "$LISTS_RAW_URL/solus_sources.txt"
 ## Link makefiles
 print_step "Linking makefiles"
 ln -srfv common/Makefile.common Makefile.common
 ln -srfv common/Makefile.iso Makefile.iso
 ln -srfv common/Makefile.toplevel Makefile
+## Clone all source repositories
+print_step "Clonning all source repositories"
+make clone
 ## Return to home
+cd ~ || exit
+
+# Package Control
+print_step "Installing package control"
+## Enter to Sublime packages folder
+enter_folder ~/.config/sublime-text-3/Installed\ Packages
+## Download the package
+wget https://packagecontrol.io/Package%20Control.sublime-package -O Package\ Control.sublime-package
+## Back to home
 cd ~ || exit
 
 # Telegram Desktop
@@ -184,17 +178,19 @@ cd ~ || exit
 # Deezloader App
 print_step "Setting-up Deezloader App"
 enter_folder ~/Git/deezloader-app
-npm install
+yarn install
 ## Back to home
 cd ~ || exit
 
 # Go packages
 print_step "Installing Go packages"
 ## Fixes
-### Export GOPATH so the Go packages installation will not explode
+### Export GOPATH so the Go packages installation will not go KABOOM!
 export GOPATH=$HOME/.golang
 ## Install packages
 go_get_list "$LISTS_RAW_URL/go_packages.txt"
+## Install gometalinter linters
+$GOPATH/bin/gometalinter --install
 
 # FINISHED!
 print_step "Script has finished! You should reboot as soon as possible"
