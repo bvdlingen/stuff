95019335f60ef6db77f792381a0c9f5521fda0c3
Federico D. Schonborn <federico.d.schonborn@gmail.com>
2017-03-22 03:14:31 -0300

    Derpy as fuck unification of bot Windows post-installs

--- windows/postinstall.ps1
+++ windows/postinstall.ps1
@@ -1,22 +1,44 @@
-Set-ExecutionPolicy -Force Unrestricted
+# Who needs sudo?
+function Invoke-AsAdmin {
+   param([string]$Command)
 
-function Remove-MetroApp {
-    param([string]$Name)
-    Get-AppXPackage -User -Name $Name | Remove-AppXPackage
-    Get-AppXPackage -AllUsers -Name $Name | Remove-AppXPackage
+   Start-Process powershell -Verb RunAs -Wait -ArgumentList "-Command '& {$Command}'"
 }
+
+# Lower execution policy
+Invoke-AsAdmin -Command "Set-ExecutionPolicy -Force Unrestricted"
 
 # Software
 ## Metro apps
-Write-Output "Removing unused Metro apps"
-Remove-MetroApp -Name *3dbuilder*
-Remove-MetroApp -Name *bing*
-Remove-MetroApp -Name *office*
-Remove-MetroApp -Name *skype*
-Remove-MetroApp -Name *xbox*
-Remove-MetroApp -Name *zune*
+Write-Output "Removing all possible Metro apps"
+Invoke-AsAdmin -Command "Get-AppXPackage -User | Remove-AppXPackage"
+Invoke-AsAdmin -Command "Get-AppXPackage -AllUsers | Remove-AppXPackage"
 ## Packages
-Write-Output "Checking if the Chocolatey provider is enabled"
-Get-PackageProvider -Name Chocolatey
+Write-Output "Installing Chocolatey package manager"
+Invoke-AsAdmin -Command "Invoke-WebRequest https://chocolatey.org/install.ps1 | Invoke-Expression"
 Write-Output "Installing packages"
-Find-Package -Name googlechrome,libreoffice,visualstudiocode,git,github,golang,nodejs,heroku-cli,7zip -ProviderName Chocolatey | Install-Package -Force
+Invoke-AsAdmin -Command "choco install -y googlechrome libreoffice visualstudiocode git github golang nodejs heroku-cli 7zip"
+
+$ListsURL = "https://raw.githubusercontent.com/feskyde/things/master/lists"
+
+function GitClone-FromRemoteList {
+    param([string]$Uri)
+    Invoke-WebRequest -Uri $Uri -OutFile list.txt
+    Get-Content list.txt | ForEach-Object {
+        git clone --recursive $_
+    }
+}
+
+function GoGet-FromRemoteList {
+    param([string]$Uri)
+    Invoke-WebRequest -Uri $Uri -OutFile list.txt
+    Get-Content list.txt | ForEach-Object {
+        go get -v -u $_
+    }
+}
+
+# Get Git repositories
+GitClone-FromRemoteList -Uri "$ListsURL/common/git_repositories.txt"
+
+# Get Go packages
+GoGet-FromRemoteList -Uri "$ListsURL/common/go_packages.txt"
