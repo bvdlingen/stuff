59e0b5f06aa8a41088f2353e35ccf0186682a99d
Federico Damián Schonborn <federicodamiansch@gmail.com>
2017-04-02 23:01:39 -0300

    Edit postinstall.ps1
    Signed-off-by: Federico Damián Schonborn <federicodamiansch@gmail.com>

--- windows/postinstall.ps1
+++ windows/postinstall.ps1
@@ -1,44 +1,29 @@
-# Who needs sudo?
-function Invoke-AsAdmin {
-   param([string]$Command)
+﻿# Functions
+function Remove-MetroApps {
+    param([string]$Regexp)
 
-   Start-Process powershell -Verb RunAs -Wait -ArgumentList "-Command '& {$Command}'"
+    Get-AppxPackage -AllUsers "*$Regexp*" | Remove-AppxPackage
 }
 
 # Lower execution policy
-Invoke-AsAdmin -Command "Set-ExecutionPolicy -Force Unrestricted"
+Set-ExecutionPolicy -Force RemoteSigned
 
-# Software
-## Metro apps
-Write-Output "Removing all possible Metro apps"
-Invoke-AsAdmin -Command "Get-AppXPackage -User | Remove-AppXPackage"
-Invoke-AsAdmin -Command "Get-AppXPackage -AllUsers | Remove-AppXPackage"
-## Packages
-Write-Output "Installing Chocolatey package manager"
-Invoke-AsAdmin -Command "Invoke-WebRequest https://chocolatey.org/install.ps1 | Invoke-Expression"
+# Packages
 Write-Output "Installing packages"
-Invoke-AsAdmin -Command "choco install -y googlechrome libreoffice visualstudiocode git github golang nodejs heroku-cli 7zip"
+## Install Chocolatey Package Manager
+Invoke-WebRequest -UseBasicParsing https://chocolatey.org/install.ps1 | Invoke-Expression
+## Refresh the environment
+refreshenv
+## Install packages... duh
+choco install -y googlechrome libreoffice visualstudiocode git golang nodejs heroku-cli 7zip
 
-$ListsURL = "https://raw.githubusercontent.com/feskyde/things/master/lists"
-
-function GitClone-FromRemoteList {
-    param([string]$Uri)
-    Invoke-WebRequest -Uri $Uri -OutFile list.txt
-    Get-Content list.txt | ForEach-Object {
-        git clone --recursive $_
-    }
-}
-
-function GoGet-FromRemoteList {
-    param([string]$Uri)
-    Invoke-WebRequest -Uri $Uri -OutFile list.txt
-    Get-Content list.txt | ForEach-Object {
-        go get -v -u $_
-    }
-}
-
-# Get Git repositories
-GitClone-FromRemoteList -Uri "$ListsURL/common/git_repositories.txt"
-
-# Get Go packages
-GoGet-FromRemoteList -Uri "$ListsURL/common/go_packages.txt"
+# Metro apps
+Write-Output "Removing metro apps"
+## Remove all
+Remove-MetroApps xbox
+Remove-MetroApps bing
+Remove-MetroApps zune
+Remove-MetroApps paint
+Remove-MetroApps 3d
+Remove-MetroApps skype
+Remove-MetroApps messaging
