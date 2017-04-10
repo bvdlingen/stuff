7fd7761e4e0639a951fc05ef0694b419cd74aa78
Federico Damián Schonborn <federicodamiansch@gmail.com>
2017-04-10 17:24:22 -0300

    Split configurations
    Signed-off-by: Federico Damián Schonborn <federicodamiansch@gmail.com>

---
+++ solus/postinstall_casa.sh
@@ -0,0 +1,74 @@
+#!/usr/bin/env bash
+#
+# Solus MATE Post Install Script (casa)
+#
+
+# Variables
+## Lists (bad design is bad)
+LISTS_RAW_URL="https://raw.githubusercontent.com/feddasch/things/master/lists"
+## Third Party specs
+SPECS_RAW_URL="https://raw.githubusercontent.com/solus-project/3rd-party/master"
+
+# Functions
+notify_step() {
+    echo -e ">> $1"
+    notify-send "Solus MATE Post Install Script (casa)" "$1" -i distributor-logo-solus
+}
+
+notify_substep() {
+    echo -e " - $1"
+}
+
+check_folder() {
+    if ! test -d "$1"; then
+        notify_substep "Creating folder: $1"
+        mkdir -pv "$1"
+    fi
+}
+
+enter_folder() {
+    check_folder "$1"
+    notify_substep "Entering to folder: $1"
+    cd "$1" || exit
+}
+
+list_tp_install() {
+    while read -r package; do
+        notify_substep "Installing third-party package: $package"
+        sudo eopkg build -y --ignore-safety "$SPECS_RAW_URL/$package/pspec.xml"
+        sudo eopkg install -y ./*.eopkg && rm -rfv ./*.eopkg
+    done < <(curl "$1")
+}
+
+# Welcome
+notify_step "Script is now running, do not touch anything until it finishes :)"
+
+# Manage repositories
+notify_step "Switching to Unstable repository"
+## Remove Solus (Shannon)
+sudo eopkg remove-repo -y Solus
+## Add Unstable
+sudo eopkg add-repo -y Solus https://packages.solus-project.com/unstable/eopkg-index.xml.xz
+
+# Manage packages
+## Remove unneded stuff
+notify_step "Removing unneded stuff"
+sudo eopkg remove -y --purge firefox thunderbird arc-firefox-theme
+## Upgrade the system
+notify_step "Getting the system up to date"
+sudo eopkg upgrade -y
+## Install third party stuff
+notify_step "Installing third party packages"
+list_tp_install "$LISTS_RAW_URL/solus/third_party.txt"
+## Install extra applications and stuff
+notify_step "Installing more packages"
+sudo eopkg install -y caja-extensions geary libreoffice-all vscode fish yadm neofetch golang \
+                      hugo git{,-extras} solbuild{,-config{,-local}-unstable} font-firacode-otf
+
+# Password-less user (EXTREMELY INSANE STUFF)
+notify_step "Setting password-less user"
+## Remove password for Casa
+sudo passwd -du casa
+## Add nullok option to PAM files
+sudo find /etc/pam.d -name "*" -exec sed -i {} -e "s:try_first_pass nullok:try_first_pass:g" \
+                                               -e "s:pam_unix.so:pam_unix.so nullok:g" \;
---
+++ solus/postinstall_feddasch.sh
@@ -0,0 +1,119 @@
+#!/usr/bin/env bash
+#
+# Solus MATE Post Install Script (feddasch)
+#
+
+# Variables
+## Lists (bad design is bad)
+LISTS_RAW_URL="https://raw.githubusercontent.com/feddasch/things/master/lists"
+## Project directory
+PROJECT_DIR="$HOME/Proyectos"
+
+# Functions
+notify_step() {
+    echo -e ">> $1"
+    notify-send "Solus MATE Post Install Script (feddasch)" "$1" -i distributor-logo-solus
+}
+
+notify_substep() {
+    echo -e " - $1"
+}
+
+check_folder() {
+    if ! test -d "$1"; then
+        notify_substep "Creating folder: $1"
+        mkdir -pv "$1"
+    fi
+}
+
+enter_folder() {
+    check_folder "$1"
+    notify_substep "Entering to folder: $1"
+    cd "$1" || exit
+}
+
+list_git_clone() {
+    while read -r repository; do
+        notify_substep "Clonning repository: $repository"
+        git clone --recursive "$repository"
+    done < <(curl "$1")
+}
+
+list_go_get() {
+    while read -r package; do
+        notify_substep "Installing Go package: $package"
+        go get -v -u "$package"
+    done < <(curl "$1")
+}
+
+# Welcome
+notify_step "Script is now running, do not touch anything until it finishes :)"
+
+# Dotfiles
+notify_step "Setting-up dotfiles"
+## Clone the repository and decrypt the binary
+yadm clone -f https://github.com/feddasch/dotfiles
+yadm decrypt
+## Link the VS Code directory to VS Code OSS directory
+ln -rsfv "$HOME/.config/Code" "$HOME/.config/Code - OSS"
+## Default to Fish
+sudo chsh -s /usr/bin/fish feddasch
+
+# Git repositories
+## Switch to the projects directory
+enter_folder "$PROJECT_DIR"
+## Clone the repositories
+notify_step "Cloning Git repositories"
+list_git_clone "$LISTS_RAW_URL/common/git_repos.txt"
+## Return to home
+cd || exit
+
+# Packaging
+## Install development packages
+notify_step "Installing development component"
+sudo eopkg install -y -c system.devel
+## Set up Solbuild
+notify_step "Setting up solbuild"
+sudo solbuild init -u
+## Create packages folder
+notify_step "Setting up Solus packages folder"
+enter_folder "$PROJECT_DIR/Solus"
+## Clone common repository
+notify_step "Clonning common repository"
+git clone https://git.solus-project.com/common
+## Link makefiles
+notify_step "Linking makefiles"
+ln -rsfv common/Makefile.toplevel Makefile
+ln -rsfv common/Makefile.common   Makefile.common
+ln -rsfv common/Makefile.iso      Makefile.iso
+## Clone all source repositories
+notify_step "Clonning all source repositories"
+make clone -j100
+## Return to home
+cd || exit
+
+# Telegram Desktop
+notify_step "Installing Telegram Desktop"
+## Enter into the Telegram folder
+enter_folder "$HOME/.TelegramDesktop"
+## Download the tarball
+wget https://tdesktop.com/linux/current?alpha=1 -O current.tar.xz
+## Unpack it
+tar xfv current.tar.xz --strip-components=1 --show-transformed-names && rm -rfv current.tar.xz
+## Back to home
+cd || exit
+
+# Go packages
+notify_step "Installing Go packages"
+## Fixes
+### Create GOPATH so the Go packages installation will not go KABOOM!
+GOPATH="$HOME/.golang"
+enter_folder "$GOPATH"
+## Install packages
+list_go_get "$LISTS_RAW_URL/common/go_packages.txt"
+## Link my repositories to the projects directory
+for dir in "$GOPATH/src/github.com/feddasch/"*; do
+    ln -rsfv "$dir" "$PROJECT_DIR/$dir"
+done
+## Back to home
+cd || exit
