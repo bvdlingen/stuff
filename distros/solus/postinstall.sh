05d4120fc08561cddf4a8ab75f2dfbd33d1a7e3a
Federico Damián Schonborn <federicodamiansch@gmail.com>
2017-04-20 01:22:13 -0300

    Merge postinstall files again
    Signed-off-by: Federico Damián Schonborn <federicodamiansch@gmail.com>

--- distros/solus/postinstall.sh
+++ distros/solus/postinstall.sh
@@ -12,6 +12,8 @@ LISTS_RAW_URL="$RAW_URL/lists"
 SCRIPTS_RAW_URL="$RAW_URL/scripts"
 ## Third Party specs
 SPECS_RAW_URL="https://raw.githubusercontent.com/solus-project/3rd-party/master"
+## Project directory
+PROJECT_DIR="$HOME/Proyectos"
 
 # Functions
 function notify_step() {
@@ -33,7 +35,7 @@ function enter_folder() {
     cd "$1" || exit
 }
 
-# Welcome
+# Start
 notify_step "Script is now running, do not touch anything until it finishes :)"
 
 # Manage repositories
@@ -46,7 +48,7 @@ sudo eopkg add-repo -y Solus https://packages.solus-project.com/unstable/eopkg-i
 # Manage packages
 ## Remove unneded stuff
 notify_step "Removing unneded stuff"
-sudo eopkg remove -y --purge firefox thunderbird arc-firefox-theme rhythmbox{,-alternative-toolbar} orca
+sudo eopkg remove -y --purge rhythmbox orca onboard
 ## Upgrade the system
 notify_step "Getting the system up to date"
 sudo eopkg upgrade -y
@@ -59,37 +61,95 @@ while read -r package; do
 done < <(curl -sL "$LISTS_RAW_URL/solus/third_party.txt")
 ## Install extra applications and stuff
 notify_step "Installing more packages"
-sudo eopkg install -y geary libreoffice-all vscode yadm fish neofetch git{,-extras} yarn golang heroku-cli \
-                      solbuild{,-config{,-local}-unstable} font-firacode-otf budgie-{screenshot,haste}-applet \
-                      sayonara-player
+sudo eopkg install -y caja-extensions sayonara-player libreoffice-all vscode yadm fish neofetch \
+                      git{,-extras} yarn golang heroku-cli solbuild{,-config{,-local}-unstable} \
+                      font-firacode-otf
+## Install development packages
+notify_step "Installing development component"
+sudo eopkg install -y -c system.devel
+## Setup solbuild
+notify_step "Setting up solbuild"
+sudo solbuild init -u
 
-# Password-less user (EXTREMELY INSANE STUFF)
-notify_step "Setting password-less user"
-## Remove user password
-sudo passwd -du "$(whoami)"
-## Add nullok option to PAM files
-sudo find /etc/pam.d/* -exec sed -i {} -e "s:shadow nullok:shadow:g" \
-                                       -e "s:pam_unix.so:pam_unix.so nullok:g" \;
-
-# User shell
-notify_step "Setting $(which fish) as default user shell"
+# Dotfiles
+notify_step "Setting-up dotfiles"
+## Clone the repository and decrypt the binary
+yadm clone -f https://github.com/feddasch/dotfiles
+yadm decrypt
+## Link the VS Code stuff to VS Code OSS's
+ln -rsfv "$HOME/.config/Code" "$HOME/.config/Code - OSS"
+## Set fish as the default shell
 sudo chsh -s "$(which fish)" "$(whoami)"
+
+# Git repositories
+## Switch to the projects directory
+enter_folder "$PROJECT_DIR"
+## Clone the repositories
+notify_step "Cloning Git repositories"
+while read -r repository; do
+    notify_substep "Clonning repository: $repository"
+    git clone "$repository"
+done < <(curl -sL "$LISTS_RAW_URL/common/git_repos.txt")
+## Return to home
+cd || exit
+
+# Packaging
+## Create packages folder
+notify_step "Setting up Solus packages folder"
+enter_folder "$PROJECT_DIR/Solus"
+## Clone common repository
+notify_step "Clonning common repository"
+git clone https://git.solus-project.com/common
+## Link makefiles
+notify_step "Linking makefiles"
+ln -rsfv common/Makefile.toplevel Makefile
+ln -rsfv common/Makefile.common   Makefile.common
+ln -rsfv common/Makefile.iso      Makefile.iso
+## Get source repositories
+notify_step "Getting source repositories"
+while read -r source; do
+    notify_substep "Getting source: $repository"
+    git clone "https://git.solus-project.com/packages/$source"
+done < <(curl -sL "$LISTS_RAW_URL/solus/package_sources.txt")
+## Return to home
+cd || exit
 
 # Stremio
 notify_step "Installing Stremio"
 # shellcheck disable=SC2024
 sudo bash < <(curl -sL "$SCRIPTS_RAW_URL/stremio.sh")
 
-# Personalization
-notify_step "Setting stuff with GSettings"
-### Privacy
-gsettings set org.gnome.desktop.privacy remove-old-temp-files true
-gsettings set org.gnome.desktop.privacy remove-old-trash-files true
-### Location
-gsettings set org.gnome.system.location enabled true
-### Sounds
-gsettings set org.gnome.desktop.sound event-sounds true
-gsettings set org.gnome.desktop.sound input-feedback-sounds true
-gsettings set org.gnome.desktop.sound theme-name "freedesktop"
-### Window manager
-gsettings set org.gnome.desktop.wm.preferences num-workspaces 1
+# Telegram Desktop
+notify_step "Installing Telegram Desktop"
+bash < <(curl -sL "$SCRIPTS_RAW_URL/tdesktop-alpha.sh")
+
+# Go packages
+notify_step "Installing Go packages"
+## Create GOPATH so the Go packages installation will not do KABOOM!
+export GOPATH="$PROJECT_DIR/Go"
+## Install the Go package list
+while read -r package; do
+    notify_substep "Installing Go package: $package"
+    go get -v -u "$package"
+done < <(curl -sL "$LISTS_RAW_URL/common/go_packages.txt")
+
+# System configuration files
+notify_step "Adding system configuration files"
+## Autologin
+sudo mkdir -pv /etc/lightdm
+echo -e "[Seat:*]\nautologin-user=$(whoami)" | sudo tee /etc/lightdm/lightdm.conf
+## Stop fucking my CPU, solbuild
+sudo mkdir -pv /etc/eopkg
+echo -e "[build]\njobs = -j3" | sudo tee /etc/eopkg/eopkg.conf
+
+# Password-less user (EXTREMELY INSANE STUFF)
+notify_step "Setting password-less user"
+## Remove user password
+sudo passwd -du "$(whoami)"
+## Add nullok option to Unix PAM module
+sudo find /etc/pam.d/* -exec sed -i {} -e "s:shadow nullok:shadow:g" \
+                                       -e "s:try_first_pass nullok:try_first_pass:g" \
+                                       -e "s:pam_unix.so:pam_unix.so nullok:g" \;
+
+# Finish
+notify_step "Done, please reboot"
