afcb0845c7499f5325fbe3e90a5e45aeb6d283b4
Federico Damián Schonborn <federicodamiansch@gmail.com>
2017-06-24 20:59:48 -0300

    Resort the files, drop the tdesktop installer script and merge it into the postinstall script
    Signed-off-by: Federico Damián Schonborn <federicodamiansch@gmail.com>

--- postinstall/solus/run.sh
+++ postinstall/solus/run.sh
@@ -10,11 +10,15 @@ THIRD_PARTY_URL="https://rawgit.com/solus-project/3rd-party/master"
 PROJECT_DIR="$HOME/Proyectos"
 ## Stuff repository location
 STUFF_REPO_DIR="$PROJECT_DIR/stuff"
+## Telegram Desktop
+TDESKTOP_URL="https://tdesktop.com/linux/current?alpha=1"
+TDESKTOP_TARBALL="tdesktop.tar.gz"
+DESKTOP_FILE_DEST="$HOME/.local/share/applications"
 
 # Functions
 function pushdir() {
     if ! test -d "$1"; then
-        if ! mkdir -pv "$1"; then
+        if ! mkdir -p "$1"; then
             exit
         fi
     fi
@@ -35,13 +39,13 @@ sudo eopkg -y upgrade
 ## Install third-party packages
 for package in "desktop/font/mscorefonts" "network/web/browser/google-chrome-stable"; do
     sudo eopkg -y build --ignore-safety "$THIRD_PARTY_URL/$package/pspec.xml"
-    sudo eopkg -y install ./*.eopkg && rm -rfv ./*.eopkg
+    sudo eopkg -y install ./*.eopkg && rm -rf ./*.eopkg
 done
 ## Install extra packages
-sudo eopkg -y install budgie-{haste,screenshot}-applet sayonara-player libreoffice-all vscode steam retroarch \
-                      fish yadm hub git-extras golang yarn docker solbuild-config{,-local}-unstable neofetch
-## Install development component
-sudo eopkg -y install -c system.devel
+sudo eopkg -y install budgie-{haste,screenshot}-applet sayonara-player libreoffice-all vscode steam \
+                      retroarch yadm fish git{,-extras} hub golang yarn heroku-cli docker neofetch
+## Install development component and solbuild configs
+sudo eopkg -y install -c system.devel solbuild-config{,-local}-unstable
 ## Setup solbuild build image
 sudo solbuild init -u
 
@@ -70,36 +74,42 @@ pushdir "$PROJECT_DIR/Solus"
 ## Clone common repository
 hub clone --depth=1 https://dev.solus-project.com/source/common
 ## Link Makefiles from common
+ln -rsfv common/Makefile.toplevel Makefile
 ln -rsfv common/Makefile.common Makefile.common
 ln -rsfv common/Makefile.iso Makefile.iso
-ln -rsfv common/Makefile.toplevel Makefile
 ## Get sources
 make clone -j100
 ## Return to the home directory
 popd || exit
 
 # Telegram Desktop
-bash "$STUFF_REPO_DIR/install/tdesktop/install.sh"
+pushdir "$HOME/.TelegramDesktop"
+## Install it
+wget "$TDESKTOP_URL" -O "$TDESKTOP_TARBALL"
+tar vxf "$TDESKTOP_TARBALL" --strip-components=1
+rm -rf "$TDESKTOP_TARBALL"
+## Put the .desktop file where it belongs
+mkdir -p "$DESKTOP_FILE_DEST"
+cp -rf "$STUFF_REPO_DIR/files/tdesktop/tdesktop.desktop" "$DESKTOP_FILE_DEST/telegramdesktop.desktop"
 
 # Docker
-## Enable it
+## Enable the service
 sudo systemctl enable docker
 ## Add the user to the docker group
 sudo usermod -aG docker "$(whoami)"
 
 # Go packages
-## Create GOPATH so the Go packages installation will not do KABOOM!
 export GOPATH="$PROJECT_DIR/Go"
 ## Install the Go installable packages
 while ISC="" read -r package || [[ -n "$package" ]]; do
-    go get -v -u "$package"
+    go get -u "$package"
 done < "$STUFF_REPO_DIR/lists/golang/install.txt"
 ## Download the Go downloadable packages
 while ISC="" read -r package || [[ -n "$package" ]]; do
-    go get -v -u -d "$package"
+    go get -u -d "$package"
 done < "$STUFF_REPO_DIR/lists/golang/download.txt"
 
 # Additional system configuration files
-## solbuild eats my CPUs, do not allow it
-sudo mkdir -pv /etc/eopkg
+## solbuild eats my CPUs, avoid that
+sudo mkdir -p /etc/eopkg
 echo -e "[build]\njobs = -j3" | sudo tee /etc/eopkg/eopkg.conf
