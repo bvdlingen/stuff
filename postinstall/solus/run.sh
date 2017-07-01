25f23fea3965331fd13a2bd75d66f9d39974b4fe
Federico Damián Schonborn <federicodamiansch@gmail.com>
2017-06-30 23:30:08 -0300

    Dunno
    Signed-off-by: Federico Damián Schonborn <federicodamiansch@gmail.com>

--- postinstall/solus/run.sh
+++ postinstall/solus/run.sh
@@ -15,14 +15,14 @@ TDESKTOP_URL="https://tdesktop.com/linux/current?alpha=1"
 TDESKTOP_TARBALL="tdesktop.tar.gz"
 
 # Functions
-function pushdir() {
+function cddir() {
     if ! test -d "$1"; then
         if ! mkdir -p "$1"; then
             exit
         fi
     fi
 
-    pushd "$1" || exit
+    cd "$1" || exit
 }
 
 # Repositories
@@ -41,10 +41,11 @@ for package in "desktop/font/mscorefonts" "network/web/browser/google-chrome-sta
     sudo eopkg -y install ./*.eopkg && rm -rf ./*.eopkg
 done
 ## Install extra packages
-sudo eopkg -y install budgie-{haste,screenshot}-applet sayonara-player libreoffice-all vscode steam \
-                      retroarch yadm fish git{,-extras} hub golang yarn heroku-cli docker neofetch
+sudo eopkg -y install budgie-{haste,screenshot}-applet geary simplescreenrecorder brasero sayonara-player \
+                      libreoffice-all vscode retroarch yadm fish git{,-extras} hub golang yarn heroku-cli \
+                      docker neofetch
 ## Install development component and solbuild configs
-sudo eopkg -y install -c system.devel solbuild-config{,-local}-unstable
+sudo eopkg -y install -c system.devel cve-check-tool solbuild-config{,-local}-unstable
 ## Setup solbuild build image
 sudo solbuild init -u
 
@@ -58,20 +59,20 @@ ln -rsfv "$HOME/.config/Code" "$HOME/.config/Code - OSS"
 sudo chsh -s "$(which fish)" "$(whoami)"
 
 # Git repositories
-pushdir "$PROJECT_DIR"
+cddir "$PROJECT_DIR"
 ## Clone the stuff repository
 hub clone stuff
 ## Clone the repositories
 while ISC="" read -r repository || [[ -n "$repository" ]]; do
     hub clone "$repository"
-done < "$STUFF_REPO_DIR/lists/git_repositories.txt"
+done < "$STUFF_REPO_DIR/lists/git/repos.txt"
 ## Return to the home directory
-popd || exit
+cd || exit
 
 # Solus packaging
-pushdir "$PROJECT_DIR/Solus"
+cddir "$PROJECT_DIR/Solus"
 ## Clone common repository
-hub clone --depth=1 https://dev.solus-project.com/source/common
+hub clone https://dev.solus-project.com/source/common
 ## Link Makefiles from common
 ln -rsfv common/Makefile.toplevel Makefile
 ln -rsfv common/Makefile.common Makefile.common
@@ -79,21 +80,20 @@ ln -rsfv common/Makefile.iso Makefile.iso
 ## Get sources
 make clone -j100
 ## Return to the home directory
-popd || exit
+cd || exit
 
 # Telegram Desktop
 ## Install it
-pushdir "$HOME/.TelegramDesktop"
+cddir "$HOME/.TelegramDesktop"
 wget "$TDESKTOP_URL" -O "$TDESKTOP_TARBALL"
-tar vxf "$TDESKTOP_TARBALL" --strip-components=1
+tar xf "$TDESKTOP_TARBALL" --strip-components=1
 rm -rf "$TDESKTOP_TARBALL"
-### Return to the home directory
-popd || exit
 ## Put the .desktop file where it belongs
-pushdir "$HOME/.local/share/applications"
+cddir "$HOME/.local/share/applications"
 cp -rf "$STUFF_REPO_DIR/files/tdesktop/tdesktop.desktop" telegramdesktop.desktop
+sed -e "s:%HOME%:$HOME:g" -i telegramdesktop.desktop
 ### Return to the home directory
-popd || exit
+cd || exit
 
 # Docker
 ## Enable the service
@@ -113,6 +113,25 @@ while ISC="" read -r package || [[ -n "$package" ]]; do
 done < "$STUFF_REPO_DIR/lists/golang/download.txt"
 
 # Additional system configuration files
+## Enable autologin for this user
+sudo mkdir -p /etc/lightdm
+echo -e "[Seat:*]\nautologin-user=$(whoami)" | sudo tee /etc/lightdm/lightdm.conf
 ## solbuild eats my CPUs, avoid that
 sudo mkdir -p /etc/eopkg
 echo -e "[build]\njobs = -j3" | sudo tee /etc/eopkg/eopkg.conf
+
+# Personalization
+## Privacy
+gsettings set org.gnome.desktop.privacy remove-old-temp-files true
+gsettings set org.gnome.desktop.privacy remove-old-trash-files true
+## Location
+gsettings set org.gnome.system.location enabled true
+## Sounds
+gsettings set org.gnome.desktop.sound event-sounds true
+gsettings set org.gnome.desktop.sound input-feedback-sounds true
+gsettings set org.gnome.desktop.sound theme-name "freedesktop"
+## Window manager
+gsettings set org.gnome.desktop.wm.preferences num-workspaces 1
+
+# Reboot
+systemctl reboot
